6e149e214a533dad72bbd80a8d4a0ba2f7d687da
DmitryNikolin <ni-ko-dim@ukr.net>
2022-10-29 12:19:43 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,63 +1,87 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
+console.log(galleryMarkup);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+gallery.addEventListener('click', onGalleryContainerClick);
+
+function createMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <a class="gallery__item" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
+                    />
+                </a>
+            `;
+        })
+        .join('');
+}
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
 // import { galleryItems } from './gallery-items.js';
 // // Change code below this line
 
-// console.log(galleryItems);
+// const galleryEl = document.querySelector('.gallery');
 
-// const gallery = document.querySelector('.gallery');
-// const galleryMarkup = createMarkup(galleryItems);
-// console.log(galleryMarkup);
+// const imageEl = galleryItems
+//     .map(
+//         ({ preview, original, description }) => `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`
+//     )
+//     .join('');
 
-// gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+// galleryEl.insertAdjacentHTML('afterbegin', imageEl);
 
-// gallery.addEventListener('click', onGalleryContainerClick);
+// galleryEl.addEventListener('click', showBigImage);
 
-// function createMarkup(galleryItems) {
-//     return galleryItems
-//         .map(({ preview, original, description }) => {
-//             return `
-//                 <a class="gallery__item" href="${original}">
-//                     <img
-//                         class="gallery__image"
-//                         src="${preview}"
-//                         alt="${description}"
-//                     />
-//                 </a>
-//             `;
-//         })
-//         .join('');
+// function showBigImage(evt) {
+//     console.log(galleryEl);
+//     // evt.preventDefault();
+
+//     if (evt.target.nodeName === 'IMG') {
+//         const lightbox = new SimpleLightbox('.gallery a', {
+//             captionsData: 'alt',
+//             captionDelay: 250,
+//         });
+//     }
 // }
 
-// function onGalleryContainerClick(event) {
-//     event.preventDefault();
-// }
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// const galleryEl = document.querySelector('.gallery');
+
+// const imageEl = galleryItems
+//     .map(
+//         ({ preview, original, description }) =>
+//             `<li><a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a></li>`
+//     )
+//     .join('');
+
+// galleryEl.insertAdjacentHTML('beforeend', imageEl);
 
 // const lightbox = new SimpleLightbox('.gallery a', {
 //     captionsData: 'alt',
 //     captionDelay: 250,
 // });
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-const galleryEl = document.querySelector('.gallery');
-
-const imageEl = galleryItems
-    .map(
-        ({ preview, original, description }) => `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`
-    )
-    .join('');
-
-galleryEl.insertAdjacentHTML('afterbegin', imageEl);
-
-galleryEl.addEventListener('click', showBigImage);
-
-function showBigImage(evt) {
-    console.log(galleryEl);
-    // evt.preventDefault();
-
-    if (evt.target.nodeName === 'IMG') {
-        const lightbox = new SimpleLightbox('.gallery a', {
-            captionsData: 'alt',
-            captionDelay: 250,
-        });
-    }
-}
