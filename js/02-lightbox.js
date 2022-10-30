3977290344f634f788583545e34023ad0803529e
DmitryNikolin <ni-ko-dim@ukr.net>
2022-10-30 13:35:29 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,32 +4,44 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
-const galleryMarkup = createMarkup(galleryItems);
+const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li>
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`)
+    .join('');
+
 console.log(galleryMarkup);
 
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-gallery.addEventListener('click', onGalleryContainerClick);
+// gallery.addEventListener('click', onGalleryContainerClick);
 
-function createMarkup(galleryItems) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `
-                <a class="gallery__item" href="${original}">
-                    <img
-                        class="gallery__image"
-                        src="${preview}"
-                        alt="${description}"
-                    />
-                </a>
-            `;
-        })
-        .join('');
-}
+// function createMarkup(galleryItems) {
+//     return galleryItems
+//         .map(({ preview, original, description }) => {
+//             return `
+//                 <li>
+//                     <a class="gallery__item" href="${original}">
+//                         <img
+//                             class="gallery__image"
+//                             src="${preview}"
+//                             alt="${description}"
+//                         />
+//                     </a>
+//                 </li>`})
+//         .join('');
+// }
 
-function onGalleryContainerClick(event) {
-    event.preventDefault();
-}
+// function onGalleryContainerClick(event) {
+//     event.preventDefault();
+// }
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
@@ -43,19 +55,17 @@ const lightbox = new SimpleLightbox('.gallery a', {
 // const galleryEl = document.querySelector('.gallery');
 
 // const imageEl = galleryItems
-//     .map(
-//         ({ preview, original, description }) => `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`
-//     )
-//     .join('');
+// .map(({ preview, original, description }) =>
+//     `<a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//     </a>`)
+// .join('');
 
-// galleryEl.insertAdjacentHTML('afterbegin', imageEl);
+// galleryEl.insertAdjacentHTML('beforeend', imageEl);
 
 // galleryEl.addEventListener('click', showBigImage);
 
 // function showBigImage(evt) {
-//     console.log(galleryEl);
-//     // evt.preventDefault();
-
 //     if (evt.target.nodeName === 'IMG') {
 //         const lightbox = new SimpleLightbox('.gallery a', {
 //             captionsData: 'alt',
@@ -64,20 +74,17 @@ const lightbox = new SimpleLightbox('.gallery a', {
 //     }
 // }
 
-
 // import { galleryItems } from './gallery-items.js';
 // // Change code below this line
 
 // const galleryEl = document.querySelector('.gallery');
 
 // const imageEl = galleryItems
-//     .map(
-//         ({ preview, original, description }) =>
-//             `<li><a class="gallery__item" href="${original}">
-//   <img class="gallery__image" src="${preview}" alt="${description}" />
-// </a></li>`
-//     )
-//     .join('');
+// .map(({ preview, original, description }) =>
+//     `<li><a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//     </a></li>`)
+// .join('');
 
 // galleryEl.insertAdjacentHTML('beforeend', imageEl);
 
